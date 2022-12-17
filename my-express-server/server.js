2941f2e1d467be9fed787cc49240c442ac5ce5f9
[Dpriyanshu[D kumar <rippedkumar4@gmail.com>
2022-12-18 00:59:23 +0530

    added about section

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,4 +2,5 @@ const express =require ("express");
 const app = express();
 
 app.get("/",function(req,res){res.send("hello world");});
+app.get("/about",function(req,res){res.send("<h1>hii my name is priyanshu kumar and i am a developer</h1>")})
 app.listen(3000,function(){console.log("server started at port 3000");});
