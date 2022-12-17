ec89a5cd5eb9102b7e5a35863dd99add069fba3d
[Dpriyanshu[D kumar <rippedkumar4@gmail.com>
2022-12-18 01:10:22 +0530

    calculator app

---
+++ calculator/calculator.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+app.get("/",function(req,res){res.send("app is working");});
+app.listen(3000,function(){console.log("server is running on port 3000 ");});
