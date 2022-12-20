7c07ceae21f50b8f3bcbf543c4fd2c2a59022adc
[Dpriyanshu[D kumar <rippedkumar4@gmail.com>
2022-12-20 18:20:55 +0530

    full calculator app

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.get("/",function(req,res){res.send("app is working");});
-app.listen(3000,function(){console.log("server is running on port 3000 ");});
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req, res) {
+  var result=Number(req.body.num1)+Number(req.body.num2);
+  res.send("the result of the calculation is "+"<b>"+result+"</b>");
+});
+
+app.listen(3000, function() {
+  console.log("server is running on port 3000 ");
+});
