f2d5d67588494c7a9d9018285a10cc0f314e5714
[Dpriyanshu[D kumar <rippedkumar4@gmail.com>
2022-12-22 23:31:03 +0530

    this is a boilerplate for weather app

---
+++ weatherProject/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get("/",function(req,res){res.sendFile(__dirname+"/index.html");});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){console.log("listenning to port 3000");});
